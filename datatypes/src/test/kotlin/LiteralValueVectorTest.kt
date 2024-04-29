4679e3d2b133d000476c21a95ea529eae8e90b5e
Tung Dang <tung.dang@otto.de>
2024-04-29 23:40:39 +0200

    add more test for literal value vector

--- datatypes/src/test/kotlin/LiteralValueVectorTest.kt
+++ datatypes/src/test/kotlin/LiteralValueVectorTest.kt
@@ -15,6 +15,7 @@ class LiteralValueVectorTest {
         val literalVector = LiteralValueVector(ArrowTypes.Int32Type, 1, size)
 
         assertEquals(literalVector.getValue(4), 1)
+        (0 until size).forEach { assertEquals(literalVector.getValue(it), 1) }
         assertEquals(literalVector.size(), size)
     }
 }
