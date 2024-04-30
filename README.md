a6e2ec59080cf21e42f30f3ef84d7ca81e19594e
Tung Dang <tung.dang@otto.de>
2024-04-30 08:44:56 +0200

    add test for schema converter

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # hqew
 
 Implementation of "How queries engine work?" for learning queries engine & kotlin.
+
+Progress: ![](https://geps.dev/progress/18)
--- datatypes/src/test/kotlin/SchemaTest.kt
+++ datatypes/src/test/kotlin/SchemaTest.kt
@@ -25,4 +25,23 @@ class SchemaTest {
         val newSchema = schema.select(selectedFieldIndices)
         assertEquals(newSchema.fields.size, selectedFieldIndices.size)
     }
+
+    @Test
+    fun `test schema converter`() {
+        val allTypes = ArrowTypes.getAll()
+        val fields = mutableListOf<Field>()
+
+        // generate a list of all possible ArrowTypes
+        allTypes.indices.forEach{ fields.add(Field(it.toString(), allTypes[it])) }
+        assertEquals(fields.size, allTypes.size)
+
+        val schema = Schema(fields)
+        val arrowSchema = schema.toArrow()
+        val origSchema = SchemaConverter.fromArrow(arrowSchema)
+
+        (0 until schema.fields.size).forEach {
+            assertEquals(schema.fields[it].name, origSchema.fields[it].name)
+            assertEquals(schema.fields[it].dataType, origSchema.fields[it].dataType)
+        }
+    }
 }
